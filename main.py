573775cea92b183e2ddc9d3dbc8ead9d42c18bb0
userzhong <userzhong0420@gmail.com>
2024-11-07 22:35:55 +0800

    测试main分支

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ def print_hi(name):
     print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
 
 
+def testmain():
+    print("test main")
+
+
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # print_hi('PyCharm')
+    testmain()
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
